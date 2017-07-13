9bd40d8cdb5028d99481ad6c2e76d7539ec91da7
uyeong <uyeong21c@gmail.com>
2017-07-13 13:29:32 +0900

    Bump v0.0.2

--- cli.js
+++ cli.js
@@ -4,7 +4,7 @@ const path = require('path');
 const program = require('commander');
 const {generate} = require('./index');
 const {args, config, override} = program
-    .version('0.0.1')
+    .version('0.0.2')
     .usage('[options] <name ...>')
     .option('-c, --config <path>', 'set a custom path to look for a config file')
     .option('-o, --override', 'set a allow files to override')
